b367205bf23e76333e05da32f5ef69cd8d8a6a3c
Amit Kumar <84862054+Amit-rajput97@users.noreply.github.com>
2023-04-23 13:54:20 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+        <section>
+            <div className="container">
+                <div className="left1"></div>
+                <div className="right">
+                    <div className="content">
+                        <h1>Listen Here</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Velit sit, doloribus perspiciatis officia repudiandae quae 
+                            magnam a suscipit ipsa quas alias ab consequatur possimus exercitationem, 
+                            et saepe quod nulla earum?
+                        </p>
+                        <Link to='' className='btn'>
+                            <button className='btn'>Listen Here</button>
+                        </Link>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="container">
+                <div className="left2"></div>
+                <div className="right">
+                    <div className="content">
+                        <h1>Speaking Here</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Velit sit, doloribus perspiciatis officia repudiandae quae 
+                            magnam a suscipit ipsa quas alias ab consequatur possimus exercitationem, 
+                            et saepe quod nulla earum?
+                        </p>
+                        <button className='btn'>Speak Here</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="container">
+                <div className="left3"></div>
+                <div className="right">
+                    <div className="content">
+                        <h1>Reading Here</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Velit sit, doloribus perspiciatis officia repudiandae quae 
+                            magnam a suscipit ipsa quas alias ab consequatur possimus exercitationem, 
+                            et saepe quod nulla earum?
+                        </p>
+                        <Link to='/Reading' className='btn'>
+                            <button className='btn'>Read Here</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="container">
+                <div className="left4"></div>
+                <div className="right">
+                    <div className="content">
+                        <h1>Writing Here</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Velit sit, doloribus perspiciatis officia repudiandae quae 
+                            magnam a suscipit ipsa quas alias ab consequatur possimus exercitationem, 
+                            et saepe quod nulla earum?
+                        </p>
+                        <Link to='/Speaking' className='btn'>
+                            <button className='btn'>Write Here</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Home;
